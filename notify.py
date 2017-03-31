d3c52bff466a2212a25f682764e86620dceb816a
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-03-31 15:45:24 -0500

    Replacing Identity by World

--- notify.py
+++ notify.py
@@ -17,7 +17,7 @@ if service_sid:
   print 'Sending a notification to identity: ' + identity
   notification = service.notifications.create(
     identity=identity,
-    body='Hello ' + identity + '!')
+    body='Hello World!')
   print notification
 else:
   print 'Please put a SID for a valid notification service in .env'
